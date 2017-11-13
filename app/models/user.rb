9da26be5f468e22c0104e48da2020a50d530a254
ShunYamada <sy@gociao.com>
2017-11-13 12:10:23 +0900

    Facebook authenticated done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook], omniauth_providers: [:twitter]
+         :omniauthable
 
    class << self
      def find_for_facebook_oauth(auth)
