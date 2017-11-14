e8fc77a2442366027d47c9a9b7b64866d445a5ce
ShunYamada <sy@gociao.com>
2017-11-14 09:29:59 +0900

    order fast

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -2,4 +2,6 @@ class Seat < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   mount_uploader :image, ImagesUploader
+
+  default_scope -> { order(created_at: :desc) }
 end
