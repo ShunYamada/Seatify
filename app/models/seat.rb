2dcc1e2a464e5012603d3debf4e7c03f32776828
ShunYamada <sy@gociao.com>
2017-11-14 13:24:07 +0900

    image debug

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,7 +1,12 @@
 class Seat < ApplicationRecord
+  include ActiveModel::Model
+  include ActiveModel::Conversion
+
   belongs_to :user
   validates :user_id, presence: true
   mount_uploader :image, ImagesUploader
 
   default_scope -> { order(created_at: :desc) }
+
+  def persisted? ; false ; end
 end
