144e6dc1e0acba1a5a88cca68ab33ee4ea0c03a1
ShunYamada <sy@gociao.com>
2017-11-21 18:28:03 +0900

    Session continued

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable and :omniauthable
   has_many :seats, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :omniauthable, :timeoutable
 
   mount_uploader :image, ImagesUploader
 
