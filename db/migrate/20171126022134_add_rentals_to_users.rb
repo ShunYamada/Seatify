61e3fe68d74a9f6215409a76cc81aded24142bce
ShunYamada <sy@gociao.com>
2017-11-26 11:32:24 +0900

    Debug

--- db/migrate/20171126022134_add_rentals_to_users.rb
+++ db/migrate/20171126022134_add_rentals_to_users.rb
@@ -1,7 +1,7 @@
 class AddRentalsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :iphone, :string
-    add_column :users, :android, :sting
+    add_column :users, :android, :string
     add_column :users, :macbook, :string
   end
 end
