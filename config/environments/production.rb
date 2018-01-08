a457811303ed825518490b3fa99cddde692736ed
ShunYamada <sy@gociao.com>
2018-01-08 12:50:56 +0900

    config.action_controller.forgery_protection_origin_check = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_controller.forgery_protection_origin_check = false
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
